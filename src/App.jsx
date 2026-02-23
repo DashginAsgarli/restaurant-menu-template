46f1db72dc2893223b5a3fbc91a6fa13d84739b4
DashginAsgarli <dashqinasgarli@gmail.com>
2026-02-24 02:50:19 +0400

    feat: add responsive hero section layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route, Link, NavLink, Outlet } from "react-router-dom";
+
+import HomePage from "./layout/homePage"
 
 function App() {
   return (
     <>
-      
+      <main>
+        <Outlet />
+      </main>
+
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
   )
 }
